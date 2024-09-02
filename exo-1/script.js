c5c7c93528fd5f80df4b2198ee96b5cc9f28dac8
Elisa <elisa.leroy@le-campus-numerique.fr>
2024-09-02 12:25:57 +0200

    exercice 1 fini

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,19 @@
 // you can write js here
-console.log('hello from file');
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+//prompt : affiche un pop up remplissable apparait, la variable prend la valeur du resultat du popup
+
+
+//let kelvins = 294;
+let celcius;
+
+function kelvinsToCelsius(kelvins){
+    return kelvins - 273;      //pour convertir des kelvins en celsius, on soustrait au kelvins 273
+}
+celcius = kelvinsToCelsius(kelvins);
+console.log(celcius);
+
+function celciusToFarenheit(celcius){
+    return  Math.floor(celcius * (9/5) + 32);   //on retourne celcius auquel on applique le calcul pour le transformer en farenheit. On met le tout dans une méthode pour le tranformer en entier (maths.floor)
+}
+let farenheit = celciusToFarenheit(celcius);
+console.log(farenheit);
