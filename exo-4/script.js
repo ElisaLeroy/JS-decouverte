38418d462a88df1faec426f05e3c10cc61549db8
Elisa <elisa.leroy@le-campus-numerique.fr>
2024-09-03 13:40:49 +0200

    exercice 4+5 fini

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,19 @@
-// you can write js here
-
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+//Utiliser une méthode pour enlever le dernier élément du tableau secretMessage.
+const removeLastElement = secretMessage.pop();
+//Utiliser une méthode pour ajouter les mots 'to' and 'program' comme des éléments distincts à la fin du tableau.
+secretMessage.push('to', 'program');
+//Changer le mot 'easily' par 'right' en accédant au bon index du tableau.
+secretMessage[6] = 'right';
+//Utiliser une méthode pour supprimer le premier élément du tableau.
+const removeFirstElement = secretMessage.shift();
+//Utiliser une méthode pour ajouter la chaine "Programming" en début de tableau.
+secretMessage.unshift('Programming');
+//Utiliser une méthode pour remplacer la série de cinq mots : 'get', 'right', 'the', 'first', 'time' par un seul mot : 'know'.
+secretMessage.splice(5, 5, "know");
+//Utiliser la méthode .join pour afficher le message secret dans la console.
+
+console.log(secretMessage.join(' '));
--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,16 @@
 // you can write js here
 
 console.log('exo-5');
+let input = 'Phrase à traduire.';
+input = input.toLowerCase();
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]) {
+            resultArray.push(input[i]);
+        }
+    }
+}
+console.log('résultat : '+resultArray);
