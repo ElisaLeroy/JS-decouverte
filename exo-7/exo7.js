de040c2ab08fb566375e87ebe9f1541877e02aa9
Elisa <elisa.leroy@le-campus-numerique.fr>
2024-09-05 12:04:15 +0200

    refonte de l'organisation du code de l'éxercice 7 + exo7 fais jusqu'à question 5

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,33 +1,4 @@
-console.log("exo-7");
-
-//console.log(jsonDatas);
-
-//Question 1 : Parcourir les articles fournis pour les afficher un à un dans la console
-console.log('Question 1');
-
-for (let product of jsonDatas) {
-    console.log(product);
-}
-
-
-
-//Question 2 : Créer un objet qui servira à traduire les types.
-console.log('Question 2');
-
-let types = {
-    car : 'voiture',
-    house : 'maison',
-    game : 'jeu',
-    videoGame : 'jeu vidéo',
-    show : 'spectacle'
-}
-console.log(types);
-
-
-
-//Question 3 : Ajouter une nouvelle clé aux objets existants pour stocker le type traduit
-console.log('Question 3');
-
+// fonctions
 let addTraductType = () => {
     for (let product of jsonDatas) {
         for (let type in types) {
@@ -37,52 +8,102 @@ let addTraductType = () => {
         }
     }
 }
+
+let catalogContainer = document.getElementById('catalog');
+
+
+let sortElementsByType = () => {
+    let textInput = document.getElementById('typeChoice');
+    catalogContainer.innerHTML = '';    //permet de remplacer le contenu de la di catalog par "" (donc rien), pour ensuite pouvoir exécuter le reste de la fonction
+
+    for (let item of jsonDatas) {
+        if (item.typeTraduction === textInput.value){
+            let product = document.getElementById('catalog');
+            product.innerHTML += `<h3>${item.name}</h3>
+            <ul>
+                <li>Type : ${item.typeTraduction}</li>
+                <li>Description : ${item.description}</li>
+                <li>Prix : ${item.price} €</li>
+                <li>Stock : ${item.quantity}</li>
+            </ul>`
+        }
+    }
+}
+
+let displayProducts = (data) => {
+    for (let item of data) {
+        let product = document.getElementById('catalog');
+        product.innerHTML += `<h3>${item.name}</h3>
+            <ul>
+                <li>Type : ${item.typeTraduction}</li>
+                <li>Description : ${item.description}</li>
+                <li>Prix : ${item.price} €</li>
+                <li>Stock : ${item.quantity}</li>
+            </ul>`
+    }
+}
+
+
+// main
+let types = {
+    car: 'voiture',
+    house: 'maison',
+    game: 'jeu',
+    videoGame: 'jeu vidéo',
+    show: 'spectacle'
+}
+
+
+//appel des fonctions
 addTraductType();
-console.log(jsonDatas);
+displayProducts(jsonDatas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Question 4 : Créer une page HTML et lister les articles depuis la donnée.
 
-console.log('Question 4');
-
-let displayProduct = (productList) => {
-    let catalog = document.getElementById('catalog');
-    for (let product of productList) {
-        catalog.innerHTML += `<h2>${product.name}</h2>
-<ul>
-    <li>Type : ${product.type}</li>
-    <li>Description : ${product.description}</li>
-    <li>Prix : ${product.price} €</li>
-    <li>Stock : ${product.quantity}</li>
-</ul>`
-    }
-}
-displayProduct(jsonDatas);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log('Question 4');
+//
+// let displayProduct = (productList) => {
+//     let catalog = document.getElementById('catalog');
+//     for (let product of productList) {
+//         catalog.innerHTML += `<h3>${product.name}</h3>
+// <ul>
+//     <li>Type : ${product.typeTraduction}</li>
+//     <li>Description : ${product.description}</li>
+//     <li>Prix : ${product.price} €</li>
+//     <li>Stock : ${product.quantity}</li>
+// </ul>`
+//     }
+// }
+// displayProduct(jsonDatas);
 
 
